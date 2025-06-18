1d76f9439289d7cb8fdb27d1cfc0953a46587096
Sarah Endicott <sarah.endicott@ec.gc.ca>
2025-06-18 09:35:38 -0400

    Add shiny test and vignette

---
+++ tests/testthat/test-run_app.R
@@ -0,0 +1,91 @@
+library(shinytest2)
+library(CaribouDemographyBasicApp)
+
+shinytest2::record_test(run_caribou_demog_app())
+
+test_that("App loads properly", {
+  skip_on_ci()
+  skip_on_covr()
+
+  shiny_app <- run_caribou_demog_app()
+  app <- AppDriver$new(shiny_app, variant = platform_variant(r_version = FALSE))
+
+  app$set_window_size(width = 1619, height = 1065)
+  app$set_inputs(
+    pop_name = "Basse-Côte-Nord",
+    cur_pop = character(0),
+    addl_params_cur = character(0),
+    ivType = "logistic",
+    S_bar = 84.0694082727134,
+    R_bar = 26.7153340763145,
+    S_iv_cv = 0.217863520723237,
+    R_iv_cv = 0.0974887866343633,
+    S_iv_sd = 1.24256062006226,
+    R_iv_sd = 0.694032575040565,
+    P_0_cur = 1,
+    P_K_cur = 0.6,
+    s_cur = 0.5,
+    N0 = 175,
+    S_sd = 0.513,
+    R_sd = 0.141,
+    a_cur = 1,
+    b_cur = 4,
+    K_cur = 10000,
+    r_max_cur = 1.3,
+    wait_ = FALSE
+  )
+  app$click("run_model")
+
+  pop_tbl <- app$get_text("#pop_table") %>% stringr::str_replace_all("    ", ";") %>%
+    stringr::str_replace_all("  ", ";") %>%
+    {readr::read_delim(., delim = ";", skip_empty_rows = TRUE)}
+
+  expect_equal(pop_tbl$` Scenario`[1], "Current")
+
+
+  app$set_window_size(width = 1619, height = 1065)
+  app$click("add_alt")
+  app$wait_for_idle()
+  app$set_inputs(alt_box_1 = "alt_box_p_1")
+  app$set_inputs(
+    alt_S_bar_1 = 92,
+    alt_R_bar_1 = 44,
+    alt_name_1 = "test1",
+    addl_params_1 = character(0),
+    P_0_1 = 1,
+    P_K_1 = 0.6,
+    s_1 = 0.5,
+    a_1 = 1,
+    b_1 = 4,
+    K_1 = 10000,
+    r_max_1 = 1.3,
+    wait_ = FALSE
+  )
+  app$click("run_model")
+  pop_tbl <- app$get_text("#pop_table") %>% stringr::str_replace_all("    ", ";") %>%
+    stringr::str_replace_all("  ", ";") %>%
+    {readr::read_delim(., delim = ";", skip_empty_rows = TRUE)}
+
+  expect_equal(pop_tbl$` Scenario`[2], "test1")
+
+})
+
+test_that("Update data works properly", {
+  skip("Skipping because not working")
+  skip_on_ci()
+  skip_on_covr()
+  skip_if_not_installed("googlesheets4")
+
+  shiny_app <- run_caribou_demog_app()
+  app <- AppDriver$new(shiny_app, variant = platform_variant(r_version = FALSE))
+  app$click("update_data")
+  app$set_inputs(survey_url = "https://docs.google.com/spreadsheets/d/1i53nQrJXgrq3B6jO0ATHhSIbibtLq5TmmFL-PxGQNm8/edit?usp=sharing", wait_ = FALSE)
+  app$click("update_data_submit", wait_ = FALSE)
+  app$wait_for_idle(timeout = 5*60*1000)
+
+  app$click("run_model", wait_ = FALSE)
+  pop_tbl <- app$get_text("#pop_table") %>% stringr::str_replace_all("    ", ";") %>%
+    stringr::str_replace_all("  ", ";") %>%
+    {readr::read_delim(., delim = ";", skip_empty_rows = TRUE)}
+
+})
